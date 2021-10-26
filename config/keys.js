51900414ff7910b9608c0e8fce51c849916c50fe
Dor Eden <jdog1075@gmail.com>
2021-10-26 10:37:42 +0300

    deleted comment

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoURI:'mongodb+srv://dor3400:13600@cluster0.xbeaj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     jwtSecret: 'coolDudes'
-    //fffffffffff
+    
 
 };
